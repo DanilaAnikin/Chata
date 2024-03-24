d459e40b38d176ca46bf0a294250de9247b13013
DanilaAnikin <danakin10502@gmail.com>
2024-03-24 02:38:47 +0100

    Finished AddPayment

--- src/components/AddPayment.vue
+++ src/components/AddPayment.vue
@@ -7,7 +7,7 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <div class="w-fit cursor-pointer flex gap-8 text-slate-200 border border-slate-200 rounded-2xl items-center justify-center px-10 py-2 hover:bg-slate-200 hover:text-slate-800 transition-all duration-500">
+  <div @click="emit('open-add-payment')" class="w-fit cursor-pointer flex gap-8 text-slate-200 border border-slate-200 rounded-2xl items-center justify-center px-10 py-2 hover:bg-slate-200 hover:text-slate-800 transition-all duration-500">
     <span class="text-lg font-bold">Add Payment</span>
     <PlusIcon class="w-8 h-8" />
   </div>
--- src/components/AddPaymentForm.vue
+++ src/components/AddPaymentForm.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { PaymentTypeFull } from '../types';
 
 const emit = defineEmits<{
     (e: 'add-payment', payment): void,
@@ -13,30 +12,49 @@ const inputDescription = ref<string>('');
 const inputCurrency = ref<'USD' | 'EUR' | 'CZK'>('USD');
 
 function addPayment() {
-  if(inputName.value && inputAmount.value && !isNaN(inputAmount.value)) {
+  if(inputName.value && inputAmount.value && !isNaN(inputAmount.value) && inputCurrency.value == 'USD' || inputCurrency.value == 'CZK' || inputCurrency.value == 'EUR') {
     emit('add-payment', { name: inputName.value, amount: inputAmount.value, description: inputDescription.value, currency: inputCurrency.value });
   }
   else {
-    alert('Wrong text')
+    alert('Wrong input')
   }
 }
 </script>
 
 <template>
-  <div class="bg-slate-900 rounded-xl p-6 text-slate-200 flex flex-col items-center gap-5">
-    <span class="text-xl">Are you sure you want to delete payment?</span>
-    <div class="flex flex-col gap-3">
-      <span class="flex gap-1">Type "I want to <p class="text-red-600">DELETE</p> payment" to delete</span>
+  <div class="w-1/5 bg-slate-900 rounded-xl p-6 text-slate-200 flex flex-col items-center gap-5">
+    <span class="text-xl">Add new payment</span>
+    <div class="flex flex-col gap-3 w-full">
       <label for="name">Name</label>
       <input
         type="text"
         id="name"
         v-model="inputName"
-        v-on:keyup.enter="addPayment()"
         class="bg-slate-950 text-slate-200 rounded-lg outline-none focus:ring-slate-700 block w-full p-3"
       >
-      <label for="Amount"></label>
-      <label for="Description">Name</label>
+      <div class="flex justify-between w-full">
+        <div class="w-1/2 flex flex-col gap-3">
+          <label for="amount">Amount</label>
+          <input
+            type="text"
+            id="amount"
+            v-model="inputAmount"
+            class="bg-slate-950 text-slate-200 rounded-lg outline-none focus:ring-slate-700 block w-full p-3"
+          >
+        </div>
+
+        <div class="w-1/3 flex flex-col gap-3">
+            <label for="currency">Currency</label>
+            <input type="text" id="currency" v-model="inputCurrency" placeholder="USD/CZK/EUR" class="placeholder:text-slate-800 bg-slate-950 text-slate-200 rounded-lg outline-none focus:ring-slate-700 block w-full p-3">
+        </div>
+      </div>
+      <label for="description">Description</label>
+      <textarea
+        type="text"
+        id="description"
+        v-model="inputDescription"
+        class="bg-slate-950 text-slate-200 rounded-lg outline-none focus:ring-slate-700 block w-full p-3"
+      />
     </div>
     <div class="flex justify-between w-full items-center">
       <span
